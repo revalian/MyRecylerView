9bc08d3fbc6da68a65e0fb22da324b51811081db
revalian <revalian2@gmail.com>
2020-11-09 20:00:30 +0700

    commit

---
+++ app/src/main/java/dicoding/com/lian/myrecyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package dicoding.com.lian.myrecyclerview
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero (
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
